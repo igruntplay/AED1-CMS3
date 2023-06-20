645fee0cb57cc28bd7ae27d7b82c1d2aae802545
igruntplay <gaspar.onesto@gmail.com>
2023-06-20 16:02:38 -0300

    me olvidé de varias cosas

--- Ej4-FilaDelBanco.py
+++ Ej4-FilaDelBanco.py
@@ -2,44 +2,52 @@ from queue import Queue
 
 # El tipo de fila debería ser Queue[int], pero la versión de python del CMS no lo soporta. Usaremos en su lugar simplemente "Queue"
 
-
-def avanzarFila(fila: Queue, min: int):
-    n: int = 1  # Número de personas en la fila
+# El tipo de fila debería ser Queue[int], pero la versión de Python del CMS no lo soporta. Usaremos en su lugar simplemente "Queue"
+def avanzarFila(fila: Queue, min: int) -> list:
+    n: int = fila.qsize()  # Número de personas en la fila
     caja1: int = 1  # Momento en el que caja1 atiende
     caja2: int = 3  # Momento en el que caja2 atiende
     caja3: int = 2  # Momento en el que caja3 atiende
     caja3_regresa: int = -1  # Momento en que la persona atendida por caja3 regresa a la fila
     persona: int = -1  # Persona atendida por caja3
     tiempo: int = 0  # Variable que controla el tiempo transcurrido
+    fila_final = []  # Fila final después de avanzar los minutos dados
 
     while tiempo <= min:  # Simulamos el paso del tiempo
         # Cada 4 minutos a partir de las 10:00, llega una nueva persona a la fila
-        if tiempo % 4 == 0 and tiempo != 0:
+        if tiempo != 0 and tiempo % 4 == 0:
             n += 1
             fila.put(n)
 
         # Verificamos si es el tiempo en que la persona atendida por Caja3 debe regresar a la fila
         if tiempo == caja3_regresa and persona != -1:
             fila.put(persona)
+            persona = -1
+            caja3_regresa = -1
 
         # Verificamos si es el tiempo para que Caja1 atienda a la siguiente persona
-        elif tiempo == caja1 and not fila.empty():
+        if tiempo == caja1 and not fila.empty():
             fila.get()
             caja1 += 10  # Caja1 puede atender a la siguiente persona en 10 minutos
 
         # Verificamos si es el tiempo para que Caja2 atienda a la siguiente persona
-        elif tiempo == caja2 and not fila.empty():
+        if tiempo == caja2 and not fila.empty():
             fila.get()
             caja2 += 4  # Caja2 puede atender a la siguiente persona en 4 minutos
 
         # Verificamos si es el tiempo para que Caja3 atienda a la siguiente persona
-        elif tiempo == caja3 and not fila.empty():
+        if tiempo == caja3 and not fila.empty() and persona == -1:
             persona = fila.get()
             caja3 += 4  # Caja3 puede atender a la siguiente persona en 4 minutos
             caja3_regresa = tiempo + 3  # La persona atendida por Caja3 regresa a la fila en 3 minutos
 
         tiempo += 1  # Avanzamos el tiempo
- 
+
+    while not fila.empty():  # Al final, trasladamos los restantes de la fila a la fila_final
+        fila_final.append(fila.get())
+
+    return fila_final
+
 if __name__ == '__main__':
   fila: Queue = Queue()
   fila_inicial: int = int(input())
